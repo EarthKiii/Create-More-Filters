fb05dc2376cdb5016d1beb83e3be88ff23395423
Jonas Charrier <jonas.charrier1@gmail.com>
2026-01-06 15:35:49 +0100

    Add: Both regex attributes

--- common/src/main/java/net/create_more_filters/CreateMoreFilters.java
+++ common/src/main/java/net/create_more_filters/CreateMoreFilters.java
@@ -1,7 +1,13 @@
 package net.create_more_filters;
 
 import com.simibubi.create.Create;
+import net.create_more_filters.attributes.ItemNameMatchesRegexAttribute;
+import net.create_more_filters.attributes.NameRegexAttribute;
+import net.create_more_filters.attributes.TagMatchesRegexAttribute;
+import net.create_more_filters.attributes.TagRegexAttribute;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,6 +19,8 @@ public class CreateMoreFilters {
 
     public static void init() {
         LOGGER.info("{} initializing! Create version: {} on platform: {}", NAME, Create.VERSION, CreateMoreFiltersExpectPlatform.platformName());
+        ItemAttributeRegistry.register("tag_regex", new TagMatchesRegexAttribute(".*Sword.*"));
+        ItemAttributeRegistry.register("name_regex", new ItemNameMatchesRegexAttribute(".*Sword.*"));
     }
 
     public static ResourceLocation id(String path) {
